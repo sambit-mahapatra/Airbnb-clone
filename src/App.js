4e2bd2282ff48dfcb5c7cff4b8650ab4d580c5bc
Sambit <sambit@Sambits-MacBook-Air.local>
2021-01-07 03:12:50 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
-
+import Home from './Home';
+import Header from './Header';
+import Footer from "./Footer";
+import { BrowserRouter as Router , Route, Link , Switch } from "react-router-dom";
+import SearchPage from "./SearchPage"
+ 
 function App() {
-  return (
+return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Router>
+        {/* Always Renders Header & FOoter , hence out of Switch */}
+        <Header/> 
+        <Switch>
+          <Route path="/search">
+            <SearchPage/>
+          </Route>
+          <Route path="/">
+            <Home/>
+          </Route>
+        </Switch>
+        <Footer/>
+      </Router>
+
+    </div> 
   );
 }
 
---
+++ src/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Footer.css'
+
+function Footer() {
+    return (
+        <div className="footer">
+          <p>© 2021 AirBnb Clone ! No rights reserved - this is a demo by Sambit</p>  
+          <p>Privacy . Terms . Sitemap . Company Details</p>
+        </div>
+    )
+}
+
+export default Footer
